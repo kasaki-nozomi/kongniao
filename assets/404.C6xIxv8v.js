50f5aa47a0863d9c421a63d03f904b3a3e77ddba
GaoPeng <peng.gao01@funplus.com>
2025-04-15 10:50:35 +0800

    kong

--- assets/404.C6xIxv8v.js
+++
@@ -1,2 +0,0 @@
-import"./lang.CpX5zCWK.js";import{c as s,o}from"./chunks/vue.DebXK3WS.js";const r=(s,o)=>{const r=s.__vccOpts||s;for(const[t,c]of o)r[t]=c;return r},t={class:"error"};const c=r({},[["render",function(r,c){return o(),s("div",t,"404")}],["__scopeId","data-v-e76f4268"]]);export{c as E,r as _};
-//# sourceMappingURL=404.C6xIxv8v.js.map
